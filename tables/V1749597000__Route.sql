e6c3d29a271cea08643cd941265d4d01c6bd69ca
Gabriel Bonatto <gabriel.justo@acad.pucrs.br>
2025-06-10 20:16:52 -0300

    Add Route table.

---
+++ tables/V1749597000__Route.sql
@@ -0,0 +1,15 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Route](
+	[RouteID] [int] IDENTITY(1,1) NOT NULL,
+	[AircraftID] [int] NOT NULL,
+	[From] [nvarchar](max) NOT NULL,
+	[To] [nvarchar](max) NOT NULL,
+	[Distance] [float] NOT NULL,
+	[Arrival] [datetime]NOT NULL,
+	[Departure] [datetime] NOT NULL,
+ CONSTRAINT [PK_Route] PRIMARY KEY CLUSTERED  ([RouteID]),
+ CONSTRAINT [FK_Route_Aircraft_AircraftID] FOREIGN KEY([AircraftID]) REFERENCES [dbo].[Aircraft]([AircraftID]) 
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
